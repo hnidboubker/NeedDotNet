724a99c3349b37174c1b179b9f288c954a525d67
HN <nidboubkerhoussine@live.fr>
2016-11-18 23:55:35 +0100

    Clean up Startup Commit

--- Src/Web/www/NeedDotNet.Web/App_Start/Startup.Auth.cs
+++ Src/Web/www/NeedDotNet.Web/App_Start/Startup.Auth.cs
@@ -1,46 +1,40 @@
 ﻿using System;
 using Microsoft.AspNet.Identity;
-using Microsoft.AspNet.Identity.EntityFramework;
-using Microsoft.AspNet.Identity.Owin;
 using Microsoft.Owin;
 using Microsoft.Owin.Security.Cookies;
-using Microsoft.Owin.Security.DataProtection;
-using Microsoft.Owin.Security.Google;
 using Microsoft.Owin.Security.OAuth;
 using Owin;
-using NeedDotNet.Web.Models;
-using NeedDotNet.Web.Providers;
 
 namespace NeedDotNet.Web
 {
     public partial class Startup
     {
-        // Enable the application to use OAuthAuthorization. You can then secure your Web APIs
-        static Startup()
-        {
-            PublicClientId = "web";
+        //// Enable the application to use OAuthAuthorization. You can then secure your Web APIs
+        //static Startup()
+        //{
+        //    PublicClientId = "web";
 
-            OAuthOptions = new OAuthAuthorizationServerOptions
-            {
-                TokenEndpointPath = new PathString("/Token"),
-                AuthorizeEndpointPath = new PathString("/Account/Authorize"),
-                Provider = new ApplicationOAuthProvider(PublicClientId),
-                AccessTokenExpireTimeSpan = TimeSpan.FromDays(14),
-                AllowInsecureHttp = true
-            };
-        }
+        //    OAuthOptions = new OAuthAuthorizationServerOptions
+        //    {
+        //        TokenEndpointPath = new PathString("/Token"),
+        //        AuthorizeEndpointPath = new PathString("/Account/Authorize"),
+        //        Provider = new ApplicationOAuthProvider(PublicClientId),
+        //        AccessTokenExpireTimeSpan = TimeSpan.FromDays(14),
+        //        AllowInsecureHttp = true
+        //    };
+        //}
 
-        public static OAuthAuthorizationServerOptions OAuthOptions { get; private set; }
+        //public static OAuthAuthorizationServerOptions OAuthOptions { get; private set; }
 
-        public static string PublicClientId { get; private set; }
+        //public static string PublicClientId { get; private set; }
 
         // For more information on configuring authentication, please visit http://go.microsoft.com/fwlink/?LinkId=301864
         public void ConfigureAuth(IAppBuilder app)
         {
-            // Configure the db context, user manager and signin manager to use a single instance per request
-            app.CreatePerOwinContext(ApplicationDbContext.Create);
-            app.CreatePerOwinContext<ApplicationUserManager>(ApplicationUserManager.Create);
-            app.CreatePerOwinContext<ApplicationSignInManager>(ApplicationSignInManager.Create);
+            //// Configure the db context, user manager and signin manager to use a single instance per request
+            //app.CreatePerOwinContext(ApplicationDbContext.Create);
+            //app.CreatePerOwinContext<ApplicationUserManager>(ApplicationUserManager.Create);
+            //app.CreatePerOwinContext<ApplicationSignInManager>(ApplicationSignInManager.Create);
 
             // Enable the application to use a cookie to store information for the signed in user
             app.UseCookieAuthentication(new CookieAuthenticationOptions
@@ -49,11 +43,11 @@ namespace NeedDotNet.Web
                 LoginPath = new PathString("/Account/Login"),
                 Provider = new CookieAuthenticationProvider
                 {
-                    // Enables the application to validate the security stamp when the user logs in.
-                    // This is a security feature which is used when you change a password or add an external login to your account.  
-                    OnValidateIdentity = SecurityStampValidator.OnValidateIdentity<ApplicationUserManager, ApplicationUser>(
-                        validateInterval: TimeSpan.FromMinutes(20),
-                        regenerateIdentity: (manager, user) => user.GenerateUserIdentityAsync(manager))
+                    //// Enables the application to validate the security stamp when the user logs in.
+                    //// This is a security feature which is used when you change a password or add an external login to your account.  
+                    //OnValidateIdentity = SecurityStampValidator.OnValidateIdentity<ApplicationUserManager, ApplicationUser>(
+                    //    validateInterval: TimeSpan.FromMinutes(20),
+                    //    regenerateIdentity: (manager, user) => user.GenerateUserIdentityAsync(manager))
                 }
             });
             // Use a cookie to temporarily store information about a user logging in with a third party login provider
@@ -68,7 +62,7 @@ namespace NeedDotNet.Web
             app.UseTwoFactorRememberBrowserCookie(DefaultAuthenticationTypes.TwoFactorRememberBrowserCookie);
 
             // Enable the application to use bearer tokens to authenticate users
-            app.UseOAuthBearerTokens(OAuthOptions);
+            app.UseOAuthBearerTokens(new OAuthAuthorizationServerOptions());
 
             // Uncomment the following lines to enable logging in with third party login providers
             //app.UseMicrosoftAccountAuthentication(
