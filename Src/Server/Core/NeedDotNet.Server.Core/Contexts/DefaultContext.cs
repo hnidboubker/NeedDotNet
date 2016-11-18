a5cb536181cd31b22986a5bb689f320fd8720a81
HN <nidboubkerhoussine@live.fr>
2016-11-19 00:57:14 +0100

    Imp Default Context Commit

--- Src/Server/Core/NeedDotNet.Server.Core/Contexts/DefaultContext.cs
+++ Src/Server/Core/NeedDotNet.Server.Core/Contexts/DefaultContext.cs
@@ -1,12 +1,21 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System.Data.Entity;
+using System.Data.Entity.ModelConfiguration.Conventions;
+using Microsoft.AspNet.Identity.EntityFramework;
+using NeedDotNet.Server.Domain.Entities;
 
 namespace NeedDotNet.Server.Core.Contexts
 {
-    public class DefaultContext
+    public class DefaultContext : IdentityDbContext<User, Role, long, UserLogin, UserRole, UserClaim>
     {
+        public DefaultContext():base("Default")
+        {
+            
+        }
+
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+            modelBuilder.Conventions.Remove<PluralizingTableNameConvention>();
+        }
     }
 }
