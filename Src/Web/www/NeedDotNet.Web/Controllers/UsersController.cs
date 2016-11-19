697d4b69d7740ed67f20b1fce8cd6d37b1eeda21
HN <nidboubkerhoussine@live.fr>
2016-11-19 02:00:02 +0100

    Impl Index Method using View Model Start Commit

--- Src/Web/www/NeedDotNet.Web/Controllers/UsersController.cs
+++ Src/Web/www/NeedDotNet.Web/Controllers/UsersController.cs
@@ -1,17 +1,42 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data.Entity;
 using System.Linq;
+using System.Threading.Tasks;
 using System.Web;
 using System.Web.Mvc;
+using NeedDotNet.Web.Models;
+using NeedDotNet.Web.Services;
 
 namespace NeedDotNet.Web.Controllers
 {
     public class UsersController : Controller
     {
-        // GET: Users
-        public ActionResult Index()
+        protected UserManager UserManager;
+
+        public UsersController(UserManager userManager)
         {
-            return View();
+            this.UserManager = userManager;
+        }
+
+        // GET: Users
+        public async Task<ActionResult> Index()
+        {
+            var results = await UserManager.Users.ToListAsync();
+            var model = new List<ArchiveUserModel>();
+            if (results.Any())
+            {
+                foreach (var result in results)
+                {
+                    var items = new ArchiveUserModel()
+                    {
+                        Id = result.Id,
+                      
+                    };
+                    model.Add(items);
+                }
+            }
+            return View(model);
         }
     }
 }
---
+++ Src/Web/www/NeedDotNet.Web/Models/ArchiveUserModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace NeedDotNet.Web.Models
+{
+    public class ArchiveUserModel
+    {
+        public long Id { get; set; }
+        public string Name { get; set; }
+        public string UserName { get; set; }
+        public string Email { get; set; }
+        public bool  IsActive { get; set; }
+        public DateTime? Created { get; set; }
+    }
+}
