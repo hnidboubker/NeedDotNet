e80d397a80053d348d9158097cdb0f74bb793ce3
HN <nidboubkerhoussine@live.fr>
2016-11-19 02:16:17 +0100

    Imp Person Repository Commit

--- Src/Web/www/NeedDotNet.Web/Services/PersonService.cs
+++ Src/Web/www/NeedDotNet.Web/Services/PersonService.cs
@@ -1,7 +1,10 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data.Entity;
 using System.Linq;
 using System.Web;
+using NeedDotNet.Server.Core.Contexts;
+using NeedDotNet.Server.Domain.Entities;
 
 namespace NeedDotNet.Web.Services
 {
@@ -20,13 +23,42 @@ namespace NeedDotNet.Web.Services
     }
 
 
-
     public interface IPersonRepository
     {
+        DefaultContext DataContext { get; }
+        void Add(Person person);
+        void Update(Person person);
+        void Remove(Person person);
     }
 
     public class PersonRepository : IPersonRepository
     {
-        
+        protected DefaultContext Context;
+        protected IDbSet<Person> DbSet;
+
+        public PersonRepository()
+        {
+            DbSet = DataContext.Set<Person>();
+        }
+
+        public DefaultContext DataContext
+        {
+            get { return Context ?? (Context = new DefaultContext()); } 
+        }
+
+        public virtual void Add(Person person)
+        {
+            DbSet.Add(person);
+        }
+
+        public virtual void Update(Person person)
+        {
+            DataContext.Entry(person).State = EntityState.Modified;
+        }
+
+        public virtual void Remove(Person person)
+        {
+            DbSet.Remove(person);
+        }
     }
 }
