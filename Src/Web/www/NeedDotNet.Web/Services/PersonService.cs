b6f2a080d01bef5afe5f81ba7250ccb4d11cc95f
HN <nidboubkerhoussine@live.fr>
2016-11-19 02:11:48 +0100

    Create Person Service & Repository Commit

---
+++ Src/Web/www/NeedDotNet.Web/Services/PersonService.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace NeedDotNet.Web.Services
+{
+    public interface IPersonService
+    {
+    }
+
+    public class PersonService : IPersonService
+    {
+        protected IPersonRepository Repository;
+
+        public PersonService(IPersonRepository repository)
+        {
+            Repository = repository;
+        }
+    }
+
+
+
+    public interface IPersonRepository
+    {
+    }
+
+    public class PersonRepository : IPersonRepository
+    {
+        
+    }
+}
